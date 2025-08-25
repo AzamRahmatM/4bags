0bdb8866680d0d9e46ab8b86353167cd8ff4f497
Azam Rahmat <azamrahmat9@gmail.com>
2025-08-25 23:11:55 +0300

    Implement Hero gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import gsap from "gsap";
 import {ScrollTrigger, SplitText} from "gsap/all";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import {useGSAP} from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">
-                Hello GSAP!
-            </h1>
-        </div>
+        <main>
+            <NavBar />
+            <Hero />
+
+        </main>
     )
 }
 export default App
